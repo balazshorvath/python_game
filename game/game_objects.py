c675289e8ba30798a1e60e5e5049eaa0b688104b
balazshorvath <balazs.peter.horvath@gmail.com>
2018-04-17 07:51:45 +0200

    game_object

--- game/game_objects.py
+++ game/game_objects.py
@@ -37,3 +37,37 @@ class Item(GameObject):
 
     def use(self, game: Game, user: GameObject):
         pass
+
+
+
+class Obstacle:
+    position: tuple
+    size: tuple
+    image_transition_time: int
+    collide: bool
+    images = []
+    __next_transition: int
+    __current_image: int
+
+    def __init__(self, **kwargs):
+        self.position = kwargs.get("position", (0, 0))
+        self.size = kwargs.get("size", (10, 10))
+        self.images = kwargs.get("images")
+        self.image_transition_time = kwargs.get("image_transition_time")
+        self.collide = kwargs.get("collide")
+
+    def update(self, dt):
+        self.__next_transition -= dt
+        if self.__next_transition <= 0:
+            self.__current_image += 1
+            if self.__current_image >= len(self.images):
+                self.__current_image = 0
+
+    def collides(self, obstacle):
+        if not self.collide:
+            return False
+
+        if self.position[0] - obstacle.size[0] >= obstacle.position[0] <= self.position[0] + self.size[0] \
+                and self.position[1] - obstacle.size[1] >= obstacle.position[1] <= self.position[1] + self.size[1]:
+            # we collide on the x axis
+            return True
